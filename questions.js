cc2d4dec7c5bf043af02fd73c448d375499640fb
Bruno Leonardo Sancho Ventura <86660522+brunoventura123@users.noreply.github.com>
2021-12-11 15:48:29 -0300

    question new

--- questions.js
+++ questions.js
@@ -1,176 +1,121 @@
 let questions = [
-    {
-        question: 'Que presente Ana levava para Samuel todo ano?',
-        options: [
-            'Rolos de Moisés',
-            'Uma roupa nova',
-            'Ofertas de ovelhas',
-            'Oléo perfumado'
-        ],
-        answer: 1
-    },
-    {
-        question: 'Quem foi primeiro rei de Israel?',
-        options: [
-            'Saul',
-            'Davi',
-            'Salomão',
-            'Moisés'
-        ],
-        answer: 0
-    },
-    {
-        question: 'Quem reclamou de Moisés ter casado com Zípora, “uma esposa cusita”?',
-        options: [
-            'Zinri e Cosbi',
-            'Miriã e Arão.',
-            'Jacó e Léia',
-            'Maria e José'
-        ],
-        answer: 1
-    },
-    {
-        question: 'Paulo se sustentava no ministério fabricando...',
-        options: [
-            'Bolsas',
-            'Tendas',
-            'Arados',
-            'Redes'
-        ],
-        answer: 1
-    },
-    {
-        question: 'Raabe escondeu os espiões no meio de hastes de linho. Onde estavam essas hastes?',
-        options: [
-            'No quarto',
-            'No curral',
-            'No campo',
-            'No terraço'
-        ],
-        answer: 3
-    },
-    {
-        question: 'Quando o marido e os filhos de Noemi morreram, ela pediu que seus amigos a chamassem por outro nome. Qual era?',
-        options: [
-            'Sara',
-            'Dara',
-            'Mara',
-            'Ester'
-        ],
-        answer: 2
-    },
-    {
-        question: 'Que instrumento musical Miriã tocava?',
-        options: [
-            'Harpa',
-            'Flauta',
-            'Pandeiro',
-            'Bateria'
-        ],
-        answer: 2
-    },
-    {
-        question: 'O anjo que apareceu para a esposa de Manoá disse que Sansão salvaria Israel das mãos de quem?',
-        options: [
-            'Os benjaminitas',
-            'Os gaditas',
-            'Os amonitas',
-            'Os filisteus.'
-        ],
-        answer: 3
-    },
-    {
-        question: 'Que novo nome um anjo deu a Jacó?',
-        options: [
-            'Israel',
-            'Isaque',
-            'Benjamim',
-            'Davi'
-        ],
-        answer: 0
-    },
-    {
-        question: 'Depois que Gideão destruiu um altar de Baal, que nome seu pai lhe deu?',
-        options: [
-            'Jerubaal',
-            'Jaaziel',
-            'Jadoque',
-            'Jadidias'
-        ],
-        answer: 0
-    },
-    {
-        question: 'O profeta Samuel disse para Saul: "----é melhor do que sacrifíco."',
-        options: [
-            'Amor',
-            'Óleo perfumado',
-            'Obedecer',
-            'Lealdade'
-        ],
-        answer: 2
-    },
-    {
-        question: 'Corá e Moisés eram da mesma tribo. Qual?',
-        options: [
-            'Judá',
-            'Levi',
-            'Rubem',
-            'Naftali'
-        ],
-        answer: 1
-    },
-    {
-        question: 'O que Arão transformou em sangue com o golpe do seu bastão?',
-        options: [
-            'Rio Nilo',
-            'Mar Vermelho',
-            'Rio Eufrates',
-            'Rio Giom'
-        ],
-        answer: 0
-    },
-    {
-        question: 'Quando a congregação cristã foi formada?',
-        options: [
-                    'Meados de 100 d.c.',
-                    'Em Pentecostes no ano 33.',
-                    'Em 1879 por Russel.',
-                    'Por Abel, no ínicio.'
-        ],
-        answer: 1
-    },
-    {
-        question: 'Quem formava o corpo governante do primeiro século?',
-        options: [
-                    'Todos os anciãos.',
-                    'Paulo, Barnabé e Silas.',
-                    'Apóstolos, menos Judas.',
-                    'Apóstolos e anciãos.'
-        ],
-        answer: 3
-    },
-    {
-        question: 'O coordenador do corpo de anciãos, o secretário e o superintendente de serviço formam ...',
-        options: [
-                    'A comimissão de serviço',
-                    'O corpo de anciãos',
-                    'A comissão judicativa',
-                    'A junta de pastoreio'
-        ],
-        answer: 0
-    },
-    {
-        question: 'A reunião do meio de semana é chamada de...',
-        options: [
-                    'Nossa Motivação de Pregar e Ensinar',
-                    'Nossa Vida e Ministério Cristão',
-                    'Nossa Habilidade de Pregar',
-                    'Imite a Cristo na maneira de Pregar'
-        ],
-         answer: 1
-    },
-   
-    
+  {
+    question: "Que presente Ana levava para Samuel todo ano?",
+    options: [
+      "Rolos de Moisés",
+      "Uma roupa nova",
+      "Ofertas de ovelhas",
+      "Oléo perfumado",
+    ],
+    answer: 1,
+  },
+  {
+    question: "Quem foi primeiro rei de Israel?",
+    options: ["Saul", "Davi", "Salomão", "Moisés"],
+    answer: 0,
+  },
+  {
+    question:
+      "Quem reclamou de Moisés ter casado com Zípora, “uma esposa cusita”?",
+    options: ["Zinri e Cosbi", "Miriã e Arão.", "Jacó e Léia", "Maria e José"],
+    answer: 1,
+  },
+  {
+    question: "Paulo se sustentava no ministério fabricando...",
+    options: ["Bolsas", "Tendas", "Arados", "Redes"],
+    answer: 1,
+  },
+  {
+    question:
+      "Raabe escondeu os espiões no meio de hastes de linho. Onde estavam essas hastes?",
+    options: ["No quarto", "No curral", "No campo", "No terraço"],
+    answer: 3,
+  },
+  {
+    question:
+      "Quando o marido e os filhos de Noemi morreram, ela pediu que seus amigos a chamassem por outro nome. Qual era?",
+    options: ["Sara", "Dara", "Mara", "Ester"],
+    answer: 2,
+  },
+  {
+    question: "Que instrumento musical Miriã tocava?",
+    options: ["Harpa", "Flauta", "Pandeiro", "Bateria"],
+    answer: 2,
+  },
+  {
+    question:
+      "O anjo que apareceu para a esposa de Manoá disse que Sansão salvaria Israel das mãos de quem?",
+    options: ["Os benjaminitas", "Os gaditas", "Os amonitas", "Os filisteus."],
+    answer: 3,
+  },
+  {
+    question: "Que novo nome um anjo deu a Jacó?",
+    options: ["Israel", "Isaque", "Benjamim", "Davi"],
+    answer: 0,
+  },
+  {
+    question:
+      "Depois que Gideão destruiu um altar de Baal, que nome seu pai lhe deu?",
+    options: ["Jerubaal", "Jaaziel", "Jadoque", "Jadidias"],
+    answer: 0,
+  },
+  {
+    question:
+      'O profeta Samuel disse para Saul: "----é melhor do que sacrifíco."',
+    options: ["Amor", "Óleo perfumado", "Obedecer", "Lealdade"],
+    answer: 2,
+  },
+  {
+    question: "Corá e Moisés eram da mesma tribo. Qual?",
+    options: ["Judá", "Levi", "Rubem", "Naftali"],
+    answer: 1,
+  },
+  {
+    question: "O que Arão transformou em sangue com o golpe do seu bastão?",
+    options: ["Rio Nilo", "Mar Vermelho", "Rio Eufrates", "Rio Giom"],
+    answer: 0,
+  },
+  {
+    question: "Quando a congregação cristã foi formada?",
+    options: [
+      "Meados de 100 d.c.",
+      "Em Pentecostes no ano 33.",
+      "Em 1879 por Russel.",
+      "Por Abel, no ínicio.",
+    ],
+    answer: 1,
+  },
+  {
+    question: "Quem formava o corpo governante do primeiro século?",
+    options: [
+      "Todos os anciãos.",
+      "Paulo, Barnabé e Silas.",
+      "Apóstolos, menos Judas.",
+      "Apóstolos e anciãos.",
+    ],
+    answer: 3,
+  },
+  {
+    question:
+      "O coordenador do corpo de anciãos, o secretário e o superintendente de serviço formam ...",
+    options: [
+      "A comissão de serviço",
+      "O corpo de anciãos",
+      "A comissão judicativa",
+      "A junta de pastoreio",
+    ],
+    answer: 0,
+  },
+  {
+    question: "A reunião do meio de semana é chamada de...",
+    options: [
+      "Nossa Motivação de Pregar e Ensinar",
+      "Nossa Vida e Ministério Cristão",
+      "Nossa Habilidade de Pregar",
+      "Imite a Cristo na maneira de Pregar",
+    ],
+    answer: 1,
+  },
 ];
 /*
 {
